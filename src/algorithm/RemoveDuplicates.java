2925b1a1d327cf9284ec508a3433b5983c92a3a9
hexia01 <hexia01@baidu.com>
2020-03-04 00:09:21 +0800

    删除字符串中的所有相邻重复项

--- src/algorithm/RemoveDuplicates.java
+++ src/algorithm/RemoveDuplicates.java
@@ -1,11 +1,11 @@
 package algorithm;
 
-/**
- * 删除排序数组中的重复项
- */
+import java.util.HashSet;
+
 public class RemoveDuplicates {
 
     /**
+     * 删除排序数组中的重复项
      *
      * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
      *
@@ -53,6 +53,8 @@ public class RemoveDuplicates {
     }
 
     /**
+     * 删除排序数组中的重复项
+     *
      * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
      *
      * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
@@ -105,8 +107,84 @@ public class RemoveDuplicates {
         return j;
     }
 
+    /**
+     * 删除字符串中的所有相邻重复项
+     *
+     * 给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+     *
+     * 在 S 上反复执行重复项删除操作，直到无法继续删除。
+     *
+     * 在完成所有重复项删除操作后返回最终的字符串。
+     *
+     * 示例：
+     *
+     * 输入："abbaca"
+     * 输出："ca"
+     * 解释：
+     * 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+     *
+     * 方法一：替换函数
+     * 我们可以用字符串自带的替换函数，由于字符串仅包含小写字母，因此只有 26 种不同的重复项。
+     *
+     * 将 aa 到 zz 的 26 种重复项放入集合中；
+     *
+     * 遍历这 26 种重复项，并用字符串的替换函数把重复项替换成空串。
+     *
+     * 注意，在进行过一次替换之后，可能会出现新的重复项。例如对于字符串 abbaca，如果替换了重复项 bb，字符串会变为 aaca，出现了新的重复项 aa。因此，上面的过程需要背重复若干次，直到字符串在一整轮替换过程后保持不变（即长度不变）为止。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/solution/shan-chu-zi-fu-chuan-zhong-de-suo-you-xiang-lin-zh/
+     * @param S
+     * @return
+     */
+    private static String removeDuplicateChar(String S) {
+        HashSet<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        for (char i = 'a'; i <= 'z'; i++) {
+            sb.setLength(0);
+            sb.append(i);
+            sb.append(i);
+            set.add(sb.toString());
+        }
+        int prevLength = -1;
+        while (prevLength != S.length()) {
+            prevLength = S.length();
+            for (String str : set) {
+                S = S.replace(str, "");
+            }
+        }
+        return S;
+    }
+
+    /**
+     * 方法二：栈
+     * 我们可以用栈来维护没有重复项的字母序列：
+     *
+     * 若当前的字母和栈顶的字母相同，则弹出栈顶的字母；
+     *
+     * 若当前的字母和栈顶的字母不同，则放入当前的字母。
+     *
+     * @param S
+     * @return
+     */
+    private static String removeDuplicateChar1(String S) {
+        StringBuilder sb = new StringBuilder();
+        int sbLength = 0;
+        for (char character : S.toCharArray()) {
+            if (sbLength != 0 && character == sb.charAt(sbLength - 1)) {
+                sb.deleteCharAt(sbLength - 1);
+                sbLength -= 1;
+            } else {
+                sb.append(character);
+                sbLength++;
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         System.out.println(removeDuplicates(nums));
+        System.out.println(removeDuplicateChar1("labbalcde"));
     }
 }
