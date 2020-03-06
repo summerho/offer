dcef4a7c600bb0546a34008d394e54dc0a2cf4ab
hexia01 <hexia01@baidu.com>
2020-03-06 18:58:45 +0800

    最大子序和 && 买卖股票的最佳时机

--- src/algorithm/MaxSubArray.java
+++ src/algorithm/MaxSubArray.java
@@ -1,4 +1,33 @@
 package algorithm;
 
+/**
+ * 最大子序和
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *
+ * https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-by-leetcode/
+ */
 public class MaxSubArray {
+
+    private static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > 0) {
+                nums[i] = nums[i] + nums[ i - 1];
+            }
+            maxSum = Math.max(maxSum, nums[i]);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
 }
